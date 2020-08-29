f872f68afb83720103dfea873d040f5e196d5cd4
banban-git <runaway_ban2@ypost.plala.or.jp>
2020-08-29 20:51:08 +0900

    ハッキングファイル追加

---
+++ Hacking.py
@@ -0,0 +1,30 @@
+import time 
+import getpass
+from itertools import product
+
+# パスワード
+target = getpass.getpass(prompt="パスワードを入力して下さい(5文字)：")
+print("あなたが入力したパスワードを解析中.....")
+# 入力可能文字
+chars = '0123456789abcdefghijklmnopqrstuvwxyz'
+# 入力可能文字数
+inputLength = 5
+
+# チェック関数
+def check(chars, repeat):
+    pws = product(chars, repeat=repeat)
+    for pwObj in pws:
+        password = ''.join(pwObj)
+        if password == target: 
+            return password
+# 実行
+start = time.time()
+pw = check(chars, inputLength)
+
+if (pw is None): 
+    print('失敗')
+else: 
+    print('パスワードが見つかりました -->', pw)
+
+finish = time.time() - start
+print(round(finish, 2) , "秒");
