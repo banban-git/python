6e65c185c19c760c8d46cd77283358fea909cdc1
banban-git <runaway_ban2@ypost.plala.or.jp>
2020-09-12 20:32:22 +0900

    ゲームファイル追加

--- YouTubeDownload.py
+++ YouTubeDownload.py
@@ -1,7 +1,8 @@
 import youtube_dl
 
 #動画URL
-download_url = 'https://www.youtube.com/watch?v=AVJAN6t6BDs&feature=youtu.be'
-#ダウンロード
+#download_url = 'https://www.youtube.com/watch?v=AVJAN6t6BDs&feature=youtu.be'
+download_url = input("YouTube動画URLを入力して下さい：")
 ydl = youtube_dl.YoutubeDL({'outtmpl': '%(title)s.%(ext)s','format':'best'})
 with ydl: result = ydl.extract_info(download_url, download=True)
+input("ダウンロード完了　(格納場所　C:\work\python\\" + result['title'] + ".mp4 )")
