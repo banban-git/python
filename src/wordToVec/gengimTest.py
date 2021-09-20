f166293329e01db1390ebe262a801d7520422b56
banban-git <runaway_ban2@ypost.plala.or.jp>
2021-09-20 19:47:58 +0900

    src/wordToVec/gengimTest.py

--- src/wordToVec/gengimTest.py
+++ src/wordToVec/gengimTest.py
@@ -4,9 +4,11 @@ import re
 model = word2vec.Word2Vec.load('wiki.vec.pt')
 
 # 
-results = model.most_similar("日本")
+#results = model.most_similar("日本")
+results = model.most_similar(positive=['人生'], negative=['金']) 
 
-results = model.most_similar(positive=['男性','おば'], negative=['女性']) 
-
+print("--------------------------------")
+print("WORD", "類似度")
+print("--------------------------------")
 for result in results:
-    print(result)
+    print(result[0], str( round(result[1] * 100, 3)) + "%")
